20a89c623ffa57afe029571b58830cd12faac6fd
Yaritza Bustamante <yaritzab99@gmail.com>
2024-10-21 11:11:16 -0400

    initial commit

---
+++ 1-9-1.js
@@ -0,0 +1,13 @@
+"use strict"
+let myInfo = {
+    name: "Pursalane Faye",
+    address: "121 Main Street",
+    city: "Benbrook",
+    state: "Texas",
+    zip: 76126
+};
+function printContract(myInfo) {
+    console.log(myInfo);
+  }
+  
+  printContract(myInfo)
