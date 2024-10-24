5293d2652a83f4eb3e39c7f2fe929c9c470bdce7
Yaritza Bustamante <yaritzab99@gmail.com>
2024-10-24 09:24:00 -0400

    move into folder

--- 2-9-1.js
+++
@@ -1,6 +0,0 @@
-"use strict"
-
-for (let count = 0; count < 7; count++) {
-    console.log("I love loops");
-}
-
--- 2-9-2.js
+++
@@ -1,7 +0,0 @@
-"use strict"
-
-let count = 0;
-while (count < 7) {
-    console.log("I love loops");
-    count++;
-}
